bf9889ce255929cd3d30e8774b6662ebaaa6faa7
Winterist <525522130@qq.com>
2018-05-10 11:41:47 +0800

    Update settings.py

--- todo_api/todo_api/settings.py
+++ todo_api/todo_api/settings.py
@@ -101,10 +101,12 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': 'TodoList',
-        # 'USER':'postgres',
-        # 'PASSWORD':'cjsdpxcyty',
+        # 'USER':'xxxxxxx',
+        # 'PASSWORD':'xxxxxxxx',
         # 'HOST':'127.0.0.1',
         # 'PORT':'5432',
+        # for a secret
+        # (0.0)
 
     }
 }
